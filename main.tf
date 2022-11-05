c0a0dd14df5f2cd05e8215f9a6efdec276ac5be9
sambasivarao <dssrctdp@gmail.com>
2022-11-05 15:15:48 +0530

    my-first-action

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "google" {
+  project     = "dssrc-test-project-367514"
+}
+
+module "bigquery" {
+  source       = "./modules/bigquery"
+  dataset_id   = "myfirstdataset"
+  dataset_name = "myfirstdataset"
+  description  = "my first dataset creating to test github actions"
+  project_id   = "dssrc-test-project-367514"
+  location     = "US"
+
+  tables = [
+    {
+      table_id = "bq_raw"
+      schema   = ""
+      time_partitioning = null
+      range_partitioning = null
+      expiration_time    = null
+      clustering         = []
+      labels = {}
+      
+    },
+        {
+      table_id = "bq_spd"
+      schema   = ""
+      time_partitioning = null
+      range_partitioning = null
+      expiration_time    = null
+      clustering         = []
+      labels = {}
+    },
+        {
+      table_id = "bq_failed"
+      schema   = ""
+      time_partitioning = null
+      range_partitioning = null
+      expiration_time    = null
+      clustering         = []
+      labels = {}
+    },
+  ]
+}
