3adb982065c308d06459ba8cfc4cbd8c36df5bba
sambasiva-rao_hmgroup <sambasiva.rao@hm.com>
2022-11-10 14:03:45 +0530

    eventarc added

--- modules/event-arc/main.tf
+++ modules/event-arc/main.tf
@@ -1,21 +1,3 @@
-# resource "google_eventarc_trigger" "primary" {
-#   name              = "${var.prefix}-${var.eventarc-name}"
-#   project           = var.project_id
-#   location          = var.region
-#   matching_criteria{
-#     attribute = "type"
-#     value     = var.event-provider
-#   }
-#   destination {
-#         cloud_run_service {
-#             service = var.event-destination
-#             region = var.region
-#         }
-#   }
-# }
-
-
-# Create an AuditLog for Cloud Storage trigger
 resource "google_eventarc_trigger" "trigger_auditlog_tf" {
   name     = "${var.prefix}-${var.eventarc-name}"
   location = var.region
